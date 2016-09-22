866f39b1d6d69ad22c8b1b481fdeea038a585453
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-09-22 14:55:46 -0500

    Bug fixes

--- slurm/slurm-setup.sh
+++ slurm/slurm-setup.sh
@@ -8,15 +8,14 @@ fi
 # setup and install repos and rpms from repos
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 rpm -Uvh http://repo.grid.iu.edu/osg/3.3/osg-3.3-el6-release-latest.rpm
-yum install -y yum-plugin-priorities
-yum install -y MySQL-python readline-devel openssl-devel munge-devel munge-libs pam-devel
-yum install -y rpm-build
-yum install -y mysql-server mysql-devel
+yum install -y --enablerepo=epel yum-plugin-priorities
+yum install -y --enablerepo=epel MySQL-python readline-devel openssl-devel munge-devel munge-libs pam-devel
+yum install -y --enablerepo=epel rpm-build
+yum install -y --enablerepo=epel mysql-server mysql-devel
 
 # build and install slurm
 wget 'http://www.schedmd.com/download/total/slurm-14.11.7.tar.bz2'
 rpmbuild -tb slurm-14.11.7.tar.bz2
-cur_dir=`pwd`
 cd /root/rpmbuild/RPMS/x86_64/
 yum install -y slurm-14.11.7-1.el6.x86_64.rpm slurm-munge-14.11.7-1.el6.x86_64.rpm  slurm-torque-14.11.7-1.el6.x86_64.rpm  slurm-sql-14.11.7-1.el6.x86_64.rpm  slurm-perlapi-14.11.7-1.el6.x86_64.rpm slurm-plugins-14.11.7-1.el6.x86_64.rpm slurm-slurmdbd-14.11.7-1.el6.x86_64.rpm
 cd -
@@ -68,7 +67,6 @@ chmod 755 /var/lib/osg
 cp ./user-vo-map /var/lib/osg/user-vo-map
 perl -pi -e "s/USERNAME/$username/g" /var/lib/osg/user-vo-map
 
-cd $cur_dir
 yum install -y gratia-probe-slurm
 echo "password" > /etc/gratia/slurm/pwfile
 chmod 600 /etc/gratia/slurm/pwfile
